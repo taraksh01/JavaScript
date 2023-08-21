eef47c16569c9c88006b4e5b77995f60e68175ae
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-08-21 17:55:47 +0530

    Learned about date in javascript

---
+++ 01_basics/07_date.js
@@ -0,0 +1,25 @@
+const date = new Date();
+
+// console.log(date);
+// console.log(date.toString());
+// console.log(date.toDateString());
+// console.log(date.toISOString());
+// console.log(date.toLocaleDateString());
+// console.log(date.toLocaleString());
+// console.log(date.toLocaleTimeString());
+// console.log(date.toTimeString());
+// console.log(date.toJSON());
+
+const createdDate = new Date(2039, 8, 29);
+// console.log((createdDate.toLocaleDateString()));
+
+// console.log(date.getDate());
+// console.log(date.getDay());
+// console.log(date.getFullYear());
+// console.log(date.getHours());
+// console.log(date.getMilliseconds());
+// console.log(date.getMinutes());
+// console.log(date.getMonth());
+// console.log(date.getSeconds());
+// console.log(date.getTime());
+console.log(date.valueOf());
