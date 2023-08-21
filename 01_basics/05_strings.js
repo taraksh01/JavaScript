1273d29e806c93befe6e958874a31e0f25622c6b
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-08-21 17:07:55 +0530

    Learned about string and its methods in javascript

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,13 @@
+let firstName = "tarak";
+let repos = 13;
+
+// console.log(firstName + number);
+
+console.log(
+  `Hello my name is ${firstName} and i have ${repos} repos in github`
+);
+
+console.log(firstName.length);
+console.log(firstName.toUpperCase());
+console.log(firstName.charAt(0));
+console.log(firstName.indexOf("y"));
