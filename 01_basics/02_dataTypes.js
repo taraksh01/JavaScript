9584036f0e4349e20248de83683f5f440d2daaab
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-08-21 14:57:26 +0530

    Learned about datatypes in JavaScript

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // Treat all codes as newer version of JS
+
+let firstName = "Tarak";
+let age = 18;
+let isLoggedIn = false;
+
+// Number =>2^52
+// BigInt => >2*52
+// String => ""
+// Boolean => true/false
+// null => standalone value
+// undefined => not yet defined
+// symbol => unique
+
+// Object
+
+console.log(typeof firstName);
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
