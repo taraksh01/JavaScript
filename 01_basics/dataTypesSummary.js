ae385603a95cee54ff42e3aea284104aee7b533b
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-08-21 16:11:57 +0530

    Datatype summary

---
+++ 01_basics/dataTypesSummary.js
@@ -0,0 +1,12 @@
+// Primitive
+
+// Types 7: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Reference/Non-Primitive
+
+// Array, Object, Function
+
+const id = Symbol(782);
+const anotherId = Symbol(782);
+
+console.log(id == anotherId);
