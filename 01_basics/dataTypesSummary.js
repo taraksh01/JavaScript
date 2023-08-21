9c9be19f0c1d601e3ed0b59e80e912f3c94895de
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-08-21 16:34:21 +0530

    Learned about stack and heap memory and where the data stores in javascript

--- 01_basics/dataTypesSummary.js
+++ 01_basics/dataTypesSummary.js
@@ -9,4 +9,23 @@
 const id = Symbol(782);
 const anotherId = Symbol(782);
 
-console.log(id == anotherId);
+// console.log(id == anotherId);
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++
+// Stack( Primitive ) || Heap( Non Primitive )
+
+let firstName = "tarak";
+let myFirstName = firstName;
+console.log(firstName);
+console.log(myFirstName);
+
+let userOne = {
+  firstName: "tarak",
+  upi: "tarak@paytm",
+};
+
+let userTwo = userOne;
+
+userTwo.upi = "someone@paytm";
+
+console.log(userOne, userTwo);
