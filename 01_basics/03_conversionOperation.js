cfe609dc64b3a6f1152f6427b22fe3ef600363eb
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-08-21 15:20:40 +0530

    Learned conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score = 33;
+let scoreString = "33";
+let string = "98kd";
+let newScore = null;
+let programmingScore = undefined;
+let isProgrammer = true;
+let something;
+
+console.table([
+  score,
+  scoreString,
+  string,
+  newScore,
+  programmingScore,
+  isProgrammer,
+  something,
+]);
+
+let scoreNumber = Number(score);
+let scoreStringNumber = Number(scoreString);
+let stringNumber = Number(string);
+let newScoreNumber = Number(newScore);
+let programmingScoreNumber = Number(programmingScore);
+let isProgrammerNumber = Number(isProgrammer);
+let somethingNumber = Number(something);
+
+console.table([
+  scoreNumber,
+  scoreStringNumber,
+  stringNumber,
+  newScoreNumber,
+  programmingScoreNumber,
+  isProgrammerNumber,
+  somethingNumber,
+]);
