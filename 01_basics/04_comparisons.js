b77e52c7a766f5a083a50f904a37998f670d7ff8
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-08-21 15:55:36 +0530

    Renamed comparisions to comparison

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,13 @@
+// console.log("2" > 1);
+// console.log("02" > 1);
+// Never compare between different datatypes
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// Equality and comparisons works differently
+// Comparison converts null to 0
+
+console.log("2" == 2); // Equality check
+console.log("2" === 2); // Strict Equality check ( checks both value and type of data )
