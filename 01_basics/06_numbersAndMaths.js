b8fd0a9a1961c515015a3559a9ec5c8f8de9926a
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-08-21 17:35:32 +0530

    Learned about number and Math and their methods in JavaScript

---
+++ 01_basics/06_numbersAndMaths.js
@@ -0,0 +1,23 @@
+const score = 298;
+// console.log(score);
+
+const newScore = new Number(98.5633);
+// console.log(newScore.valueOf());
+// console.log(newScore.toExponential(2));
+// console.log(newScore.toFixed(2));
+// console.log(newScore.toPrecision(3));
+// console.log(newScore.toString());
+
+const hundreds = 1000000000;
+// console.log(hundreds.toLocaleString("en-IN"));
+
+
+// +++++++++++++++++++++ Math +++++++++++++++++++++++++++++++
+
+console.log(Math.abs(-28));
+console.log(Math.ceil(782.2892));
+console.log(Math.floor(7.2873));
+console.log(Math.fround(982.379));
+console.log(Math.random());
+console.log(Math.round(2783.278));
+console.log(Math.sign(78));
