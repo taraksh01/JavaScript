d4f8a42281f3a520094f2d15c2633cc3795896cc
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-08-22 10:02:29 +0530

    Learned about Array and its methods

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,54 @@
+const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const myHeros = ["IronMan", "BatMan", "Thor", "LOKI", "SpiderMan"];
+const arr2 = [1, 2, 3, 4, "tarak", true];
+
+// console.table([arr, arr2, myHeros]);
+// console.log(myHeros[1]);
+
+// Array methods
+// console.log(arr.push(10));
+// console.log(arr);
+// console.log(arr.pop());
+// console.log(arr);
+
+// console.log(arr.unshift(-1));
+// console.log(arr);
+// console.log(arr.shift());
+// console.log(arr);
+
+// console.log(arr.concat(arr2));
+// console.log(arr2);
+// console.log(arr);
+
+// console.log(arr2);
+// console.log(arr2.copyWithin(1,2,2));
+// console.log(arr2);
+
+// console.log(arr.entries([1,4]));
+
+// console.log(arr.fill(23));
+// console.log(arr);
+
+// console.log(arr.includes(2));
+// console.log(arr.indexOf(2));
+
+// console.log(arr.join());
+
+// console.log(arr.keys());
+
+// console.log(arr.lastIndexOf(9));
+
+// console.log(arr.reverse());
+
+// console.log(arr.splice(1, 5));
+// console.log(arr);
+// console.log(arr.slice(1, 5));
+// console.log(arr);
+
+// console.log(arr.sort());
+
+// console.log(arr.toLocaleString());
+// console.log(arr.toString());
+
+// console.log(Array.from("TARAK"));
+// console.log(Array.of(1, 2, 4, 4, 3, 2, 2));
