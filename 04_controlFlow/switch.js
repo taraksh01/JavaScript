64615cf2c81e806c8d3034c6181bd75ed425534d
Tarak Shaw <135436617+taraksh01@users.noreply.github.com>
2023-08-25 09:28:21 +0530

    Learned about switch case in javascript

---
+++ 04_controlFlow/switch.js
@@ -0,0 +1,30 @@
+// switch (key) {
+//   case value:
+
+//     break;
+
+//   default:
+//     break;
+// }
+
+const month = 3;
+switch (month) {
+  case 1:
+    console.log("january");
+    break;
+  case 2:
+    console.log("february");
+    break;
+  case 3:
+    console.log("march");
+    break;
+  case 4:
+    console.log("april");
+    break;
+  case 5:
+    console.log("may");
+    break;
+
+  default:
+    break;
+}
